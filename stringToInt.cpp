9833136fa635acf1bceb00bf1d100cfbdbbdf3cd
Ashish Sharma <sharma.ashish778855@gmail.com>
2017-08-11 00:20:39 +0530

    Added stringToInt file

---
+++ stringToInt.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<sstream>
+#define ll long long
+#define dbg(x) cerr<<" x is "<<#x<<endl;
+using namespace std;
+int main()
+{
+    ll i,j,k,l,n,m;
+    string s;
+    cin>>s;
+    stringstream strstm(s);
+    int x;
+    strstm>>x;
+    cout<<x<<endl;
+    return 0;
+}
